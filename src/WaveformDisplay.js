29c3a5af5a6d6e762121129afc63043bd7ee4ea0
Ben Wiley <therealbenwiley@gmail.com>
2021-10-25 10:36:12 -0400

    Replace WaveformDisplay render with more-efficient canvas implementation.

--- src/WaveformDisplay.js
+++ src/WaveformDisplay.js
@@ -1,76 +1,114 @@
-import React from 'react';
+import React, { useImperativeHandle, useLayoutEffect, useRef } from 'react';
 import { styled } from 'tonami';
+import ResizeObserver from 'resize-observer-polyfill';
 
 import { GROUP_PIXEL_WIDTH } from './utils/waveform.js';
 
-const WaveformDiv = styled.div({
+const WaveformCanvas = styled.canvas({
   width: '100%',
   height: '100%',
-  position: 'relative',
-  overflow: 'hidden',
-});
-
-const WaveformSection = styled.div({
-  width: '100%',
-  height: ({ $positive }) => ($positive ? '67%' : '33%'),
-  display: 'flex',
-  alignItems: ({ $positive }) => ($positive ? 'flex-end' : 'flex-start'),
-});
-
-const Scaled = styled.div({
-  width: '100%',
-  height: ({ $scaleCoefficient }) => `${100 * $scaleCoefficient}%`,
-  willChange: 'height',
-  display: 'flex',
-  alignItems: ({ $positive }) => ($positive ? 'flex-end' : 'flex-start'),
-});
-
-const Bar = styled.div({
-  width: `${GROUP_PIXEL_WIDTH}px`,
-  height: ({ $amplitude, $positive }) =>
-    `${100 * ($positive ? 1 : -1) * $amplitude}%`,
-  minHeight: ({ $positive }) => ($positive ? '1px' : 0),
-  paddingRight: '1px',
-});
-
-const BarInner = styled.div({
-  backgroundColor: ({ $positive }) => ($positive ? 'red' : 'darkred'),
-  height: '100%',
+  display: 'block',
 });
 
 /**
- * @typedef {React.PropsWithChildren<{
+ * @param {HTMLCanvasElement} canvas
+ */
+function observeCanvas(canvas) {
+  /**
+   * @param {number} width
+   * @param {number} height
+   */
+  function setCanvasSize(width, height) {
+    canvas.width = width;
+    canvas.height = height;
+  }
+  setCanvasSize(canvas.offsetWidth, canvas.offsetHeight);
+  const observer = new ResizeObserver(([entry]) =>
+    setCanvasSize(entry.contentRect.width, entry.contentRect.height)
+  );
+  observer.observe(canvas);
+  return () => observer.disconnect();
+}
+
+/**
+ * @param {HTMLCanvasElement} canvas
+ * @param {import('./utils/waveform.js').SamplePeaks} peaks
+ * @param {number} scaleCoefficient
+ */
+function drawWaveform(canvas, peaks, scaleCoefficient) {
+  const ctx = /** @type {CanvasRenderingContext2D} */ (canvas.getContext('2d'));
+  const { width, height } = canvas;
+  ctx.clearRect(0, 0, width, height);
+  const positiveHeight = Math.floor(height * (2 / 3)) + 1;
+  ctx.fillStyle = 'red';
+  peaks.positive.forEach((peak, i) => {
+    const basePeakHeight = positiveHeight * peak; // float
+    // make the positive bar always at least 1px tall to avoid empty sections
+    const scaledPeakHeight = Math.max(
+      Math.round(scaleCoefficient * basePeakHeight),
+      1
+    );
+    ctx.fillRect(
+      i * GROUP_PIXEL_WIDTH,
+      positiveHeight - scaledPeakHeight,
+      GROUP_PIXEL_WIDTH - 1,
+      scaledPeakHeight
+    );
+  });
+  const negativeHeight = height - positiveHeight;
+  ctx.fillStyle = 'darkred';
+  peaks.negative.forEach((peak, i) => {
+    const basePeakHeight = negativeHeight * peak * -1; // float
+    const scaledPeakHeight = Math.round(scaleCoefficient * basePeakHeight);
+    ctx.fillRect(
+      i * GROUP_PIXEL_WIDTH,
+      positiveHeight,
+      GROUP_PIXEL_WIDTH - 1,
+      scaledPeakHeight
+    );
+  });
+}
+
+/**
+ * @typedef {{
  *   peaks: import('./utils/waveform').SamplePeaks;
  *   scaleCoefficient: number;
- *   waveformRef?: React.Ref<HTMLDivElement>;
- * }>} WaveformProps
+ *   waveformRef?: React.Ref<HTMLElement | null>;
+ * }} WaveformProps
  */
 
-const WaveformDisplay = React.memo(
+/**
+ * @param {WaveformProps} props
+ */
+function WaveformDisplayCanvas({ peaks, scaleCoefficient, waveformRef }) {
   /**
-   * @param {WaveformProps} props
+   * @type {React.RefObject<HTMLCanvasElement>}
    */
-  ({ peaks, scaleCoefficient, waveformRef, children }) => {
-    return (
-      <WaveformDiv ref={waveformRef}>
-        {[true, false].map((positive) => {
-          const key = positive ? 'positive' : 'negative';
-          return (
-            <WaveformSection key={key} $positive={positive}>
-              <Scaled $scaleCoefficient={scaleCoefficient} $positive={positive}>
-                {[].map.call(peaks[key], (amplitude, index) => (
-                  <Bar key={index} $amplitude={amplitude} $positive={positive}>
-                    <BarInner $positive={positive} />
-                  </Bar>
-                ))}
-              </Scaled>
-            </WaveformSection>
-          );
-        })}
-        {children}
-      </WaveformDiv>
-    );
-  }
-);
+  const canvasRef = useRef(null);
+  useImperativeHandle(waveformRef, () => canvasRef.current);
+  useLayoutEffect(() => {
+    if (!canvasRef.current) {
+      throw new Error('Canvas should be defined');
+    }
+    return observeCanvas(canvasRef.current);
+  }, []);
+  useLayoutEffect(() => {
+    const canvas = canvasRef.current;
+    if (!canvas) {
+      throw new Error('Canvas should be defined');
+    }
+    // wait for animation frame..
+    let frame = requestAnimationFrame(() => {
+      // just before animation frame..
+      frame = requestAnimationFrame(() => {
+        // after animation frame..
+        // for some reason the draw doesn't reliably show up before this
+        drawWaveform(canvas, peaks, scaleCoefficient);
+      });
+    });
+    return () => cancelAnimationFrame(frame);
+  }, [peaks, scaleCoefficient]);
+  return <WaveformCanvas ref={canvasRef} />;
+}
 
-export default WaveformDisplay;
+export default WaveformDisplayCanvas;

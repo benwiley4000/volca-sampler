974d6a427b02daec18d478af7a3e775f096341b0
Ben Wiley <therealbenwiley@gmail.com>
2021-10-25 23:12:06 -0400

    Wait to render waveform in list until visible (closes #18).

--- src/SampleList.js
+++ src/SampleList.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useLayoutEffect, useRef, useState } from 'react';
 import { styled } from 'tonami';
 import { WAVEFORM_CACHED_WIDTH } from './utils/waveform';
 import WaveformDisplay from './WaveformDisplay';
@@ -8,7 +8,7 @@ const SampleListContainer = styled.div({
   overflow: 'auto',
 });
 
-const SampleListItem = styled.div({
+const SampleListItemDiv = styled.div({
   padding: '0.5rem',
   border: '1px solid grey',
   cursor: 'pointer',
@@ -19,6 +19,73 @@ const WaveformContainer = styled.div({
   width: `${WAVEFORM_CACHED_WIDTH}px`,
   height: '40px',
 });
+
+const intersectionObserverAvailable =
+  typeof IntersectionObserver !== 'undefined';
+
+const SampleListItem = React.memo(
+  /**
+   * @param {{
+   *   sample: import('./store').SampleContainer;
+   *   selected: boolean;
+   *   readonly: boolean;
+   *   onSampleSelect: (id: string) => void;
+   * }} props
+   */
+  ({ sample, selected, readonly, onSampleSelect }) => {
+    /**
+     * @type {React.RefObject<HTMLDivElement>}
+     */
+    const waveformContainerRef = useRef(null);
+    const [waveformSeen, setWaveformSeen] = useState(
+      !intersectionObserverAvailable
+    );
+    useLayoutEffect(() => {
+      if (!intersectionObserverAvailable) {
+        return;
+      }
+      const waveformContainer = waveformContainerRef.current;
+      if (!waveformContainer) {
+        throw new Error('Waveform container should be defined');
+      }
+      // check if waveform is visible now.. if so, render immediately
+      const rect = waveformContainer.getBoundingClientRect();
+      if (
+        rect.top + rect.height >= 0 &&
+        rect.bottom - rect.height <= window.innerHeight
+      ) {
+        setWaveformSeen(true);
+        return;
+      }
+      // otherwise set up an observer for when the waveform does become visible
+      const observer = new IntersectionObserver(([entry]) => {
+        if (entry.isIntersecting) {
+          setWaveformSeen(true);
+          observer.unobserve(waveformContainer);
+        }
+      });
+      observer.observe(waveformContainer);
+      return () => observer.disconnect();
+    }, []);
+    return (
+      <SampleListItemDiv
+        selected={selected}
+        data-disabled={readonly}
+        onClick={() => !readonly && onSampleSelect(sample.id)}
+      >
+        <div>{sample.metadata.name}</div>
+        <WaveformContainer ref={waveformContainerRef}>
+          {waveformSeen && (
+            <WaveformDisplay
+              peaks={sample.metadata.trim.waveformPeaks}
+              scaleCoefficient={sample.metadata.scaleCoefficient}
+            />
+          )}
+        </WaveformContainer>
+      </SampleListItemDiv>
+    );
+  }
+);
 
 /**
  * @param {{
@@ -38,27 +105,20 @@ function SampleList({
 }) {
   return (
     <SampleListContainer>
-      <SampleListItem
+      <SampleListItemDiv
         data-disabled={readonly}
         onClick={() => !readonly && onNewSample()}
       >
         New Sample
-      </SampleListItem>
+      </SampleListItemDiv>
       {[...samples].map(([id, sample]) => (
         <SampleListItem
           key={id}
-          $selected={id === selectedSampleId}
-          data-disabled={readonly}
-          onClick={() => !readonly && onSampleSelect(id)}
-        >
-          <div>{sample.metadata.name}</div>
-          <WaveformContainer>
-            <WaveformDisplay
-              peaks={sample.metadata.trim.waveformPeaks}
-              scaleCoefficient={sample.metadata.scaleCoefficient}
-            />
-          </WaveformContainer>
-        </SampleListItem>
+          sample={sample}
+          selected={id === selectedSampleId}
+          readonly={readonly}
+          onSampleSelect={onSampleSelect}
+        />
       ))}
     </SampleListContainer>
   );

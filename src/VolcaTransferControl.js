fa62281bb9c35847ea6e46d7d84b26c277720441
Ben Wiley <therealbenwiley@gmail.com>
2021-10-28 11:41:29 -0400

    Precache syro stream (closes #11).

--- src/VolcaTransferControl.js
+++ src/VolcaTransferControl.js
@@ -14,11 +14,50 @@ import { getSampleBuffer } from './utils/syro.js';
 function VolcaTransferControl({ sample }) {
   const [syroProgress, setSyroProgress] = useState(0);
   const [syroTransferState, setSyroTransferState] = useState(
-    /** @type {'loading' | 'transferring' | 'error' | 'idle'} */ ('idle')
+    /** @type {'idle' | 'transferring' | 'error'} */ ('idle')
   );
-  // to be set when transfer is started
+  const [syroAudioBuffer, setSyroAudioBuffer] = useState(
+    /** @type {AudioBuffer | Error | null} */ (null)
+  );
+  // to be set when transfer or playback is started
   const stop = useRef(() => {});
   useEffect(() => {
+    let cancelled = false;
+    setSyroProgress(0);
+    setSyroTransferState('idle');
+    setSyroAudioBuffer(null);
+    stop.current = () => {
+      cancelled = true;
+    };
+    try {
+      const { sampleBufferPromise, cancelWork } = getSampleBuffer(
+        sample,
+        (progress) => {
+          if (!cancelled) {
+            setSyroProgress(progress);
+          }
+        }
+      );
+      stop.current = () => {
+        cancelWork();
+        cancelled = true;
+      };
+      sampleBufferPromise.then(async (sampleBuffer) => {
+        if (cancelled) {
+          return;
+        }
+        stop.current = () => {
+          cancelled = true;
+        };
+        const audioBuffer = await getAudioBufferForAudioFileData(sampleBuffer);
+        if (!cancelled) {
+          setSyroAudioBuffer(audioBuffer);
+        }
+      });
+    } catch (err) {
+      console.error(err);
+      setSyroAudioBuffer(new Error(String(err)));
+    }
     return () => stop.current();
   }, [sample]);
   const { playAudioBuffer, isAudioBusy } = useAudioPlaybackContext();
@@ -26,42 +65,15 @@ function VolcaTransferControl({ sample }) {
     <>
       <button
         type="button"
-        onClick={async () => {
+        onClick={() => {
+          if (!(syroAudioBuffer instanceof AudioBuffer)) {
+            return;
+          }
           try {
-            let cancelled = false;
-            stop.current = () => {
-              setSyroTransferState('idle');
-              cancelled = true;
-            };
-            setSyroProgress(0);
-            setSyroTransferState('loading');
-            const { sampleBufferPromise, cancelWork } = getSampleBuffer(
-              sample,
-              (progress) => {
-                if (!cancelled) {
-                  setSyroProgress(progress);
-                }
-              }
-            );
-            stop.current = () => {
-              cancelWork();
-              setSyroTransferState('idle');
-              cancelled = true;
-            };
-            const sampleBuffer = await sampleBufferPromise;
-            if (cancelled) {
-              return;
-            }
             setSyroTransferState('transferring');
-            const audioBuffer = await getAudioBufferForAudioFileData(
-              sampleBuffer
-            );
-            if (cancelled) {
-              return;
-            }
-            const stopPlayback = playAudioBuffer(audioBuffer, {
+            const stopPlayback = playAudioBuffer(syroAudioBuffer, {
               onTimeUpdate: (currentTime) =>
-                setSyroProgress(currentTime / audioBuffer.duration),
+                setSyroProgress(currentTime / syroAudioBuffer.duration),
               onEnded: () => setSyroTransferState('idle'),
             });
             stop.current = () => {
@@ -75,21 +87,24 @@ function VolcaTransferControl({ sample }) {
         }}
         disabled={
           isAudioBusy ||
-          syroTransferState === 'loading' ||
+          !syroAudioBuffer ||
           syroTransferState === 'transferring'
         }
       >
         transfer to volca sample
       </button>
       <br />
-      {syroTransferState === 'idle' ? null : syroTransferState === 'error' ? (
+      {syroAudioBuffer &&
+      syroTransferState === 'idle' ? null : syroTransferState === 'error' ? (
         'Error transferring'
       ) : (
         <>
           <p>
-            {syroTransferState === 'loading'
-              ? 'Preparing sample transfer...'
-              : 'Transferring to Volca Sample...'}
+            {!syroAudioBuffer
+              ? 'Preparing sample for transfer...'
+              : syroAudioBuffer instanceof AudioBuffer
+              ? 'Transferring to Volca Sample...'
+              : 'Error preparing sample for transfer'}
           </p>
           <progress value={syroProgress} />
           <br />

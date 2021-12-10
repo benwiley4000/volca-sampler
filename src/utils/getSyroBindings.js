3e74e3925ff6323650d9b8b687c81adaff6f7360
Ben Wiley <therealbenwiley@gmail.com>
2021-12-10 05:06:54 -0500

    Stop re-instantiating syro bindings each time they're used.

--- src/utils/getSyroBindings.js
+++ src/utils/getSyroBindings.js
@@ -41,69 +41,61 @@ export async function getSyroBindings() {
       'Expected CREATE_SYRO_BINDINGS global function to exist'
     );
   }
-  const Module = await window.CREATE_SYRO_BINDINGS();
   return (syroBindingsPromise =
     syroBindingsPromise ||
-    new Promise((resolve, reject) => {
+    window.CREATE_SYRO_BINDINGS().then(async (Module) => {
       /**
        * @type {SyroBindings}
        */
-      let syroBindings;
-      try {
-        syroBindings = {
-          prepareSampleBufferFromWavData: Module.cwrap(
-            'prepareSampleBufferFromWavData',
-            'number',
-            ['array', 'number', 'number', 'number', 'number']
-          ),
-          // TODO: put this back when C function works correctly
-          // prepareSampleBufferFrom16BitPcmData: Module.cwrap(
-          //   'prepareSampleBufferFrom16BitPcmData',
-          //   'number',
-          //   ['array', 'number', 'number', 'number', 'number', 'number']
-          // ),
-          prepareSampleBufferFrom16BitPcmData() {
-            throw new Error(
-              'This function does not work. Use prepareSampleBufferFromWavData.'
-            );
-          },
-          getSampleBufferChunkPointer: Module.cwrap(
-            'getSampleBufferChunkPointer',
-            'number',
-            ['number']
-          ),
-          getSampleBufferChunkSize: Module.cwrap(
-            'getSampleBufferChunkSize',
-            'number',
-            ['number']
-          ),
-          getSampleBufferProgress: Module.cwrap(
-            'getSampleBufferProgress',
-            'number',
-            ['number']
-          ),
-          getSampleBufferTotalSize: Module.cwrap(
-            'getSampleBufferTotalSize',
-            'number',
-            ['number']
-          ),
-          cancelSampleBufferWork: Module.cwrap('cancelSampleBufferWork', null, [
-            'number',
-          ]),
-          registerUpdateCallback(cb) {
-            return Module.addFunction(cb, 'vi');
-          },
-          unregisterUpdateCallback(pointer) {
-            Module.removeFunction(pointer);
-          },
-          heap8Buffer() {
-            return Module.HEAP8.buffer;
-          },
-        };
-      } catch (err) {
-        reject(err);
-        return;
-      }
-      resolve(syroBindings);
+      return {
+        prepareSampleBufferFromWavData: Module.cwrap(
+          'prepareSampleBufferFromWavData',
+          'number',
+          ['array', 'number', 'number', 'number', 'number']
+        ),
+        // TODO: put this back when C function works correctly
+        // prepareSampleBufferFrom16BitPcmData: Module.cwrap(
+        //   'prepareSampleBufferFrom16BitPcmData',
+        //   'number',
+        //   ['array', 'number', 'number', 'number', 'number', 'number']
+        // ),
+        prepareSampleBufferFrom16BitPcmData() {
+          throw new Error(
+            'This function does not work. Use prepareSampleBufferFromWavData.'
+          );
+        },
+        getSampleBufferChunkPointer: Module.cwrap(
+          'getSampleBufferChunkPointer',
+          'number',
+          ['number']
+        ),
+        getSampleBufferChunkSize: Module.cwrap(
+          'getSampleBufferChunkSize',
+          'number',
+          ['number']
+        ),
+        getSampleBufferProgress: Module.cwrap(
+          'getSampleBufferProgress',
+          'number',
+          ['number']
+        ),
+        getSampleBufferTotalSize: Module.cwrap(
+          'getSampleBufferTotalSize',
+          'number',
+          ['number']
+        ),
+        cancelSampleBufferWork: Module.cwrap('cancelSampleBufferWork', null, [
+          'number',
+        ]),
+        registerUpdateCallback(cb) {
+          return Module.addFunction(cb, 'vi');
+        },
+        unregisterUpdateCallback(pointer) {
+          Module.removeFunction(pointer);
+        },
+        heap8Buffer() {
+          return Module.HEAP8.buffer;
+        },
+      };
     }));
 }

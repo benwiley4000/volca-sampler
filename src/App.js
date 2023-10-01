ca5751c7063e611f441cdf351374c1a7ee564e37
Ben Wiley <therealbenwiley@gmail.com>
2023-10-01 09:55:31 -0400

    Revert "Another mobile layout fix."
    This reverts commit 4e8746ef7f933c38fc466de3331dc3c90656b519.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, {
   useCallback,
   useEffect,
-  useLayoutEffect,
   useMemo,
   useRef,
   useState,
@@ -211,22 +210,20 @@ function App() {
     /** @type {'sampleList' | 'currentSample' | 'about'} */ ('currentSample')
   );
 
-  useLayoutEffect(() => {
-    setSelectedMobilePage('currentSample');
-  }, [focusedSampleId]);
-
   const handleSampleSelect = useCallback(
     /**
      * @param {string | null} sampleId
      */
     (sampleId) => {
       setFocusedSampleId(sampleId);
+      setSelectedMobilePage('currentSample');
     },
     []
   );
 
   const handleHeaderClick = useCallback(() => {
     setFocusedSampleId(null);
+    setSelectedMobilePage('currentSample');
   }, []);
 
   const sample = focusedSampleId ? allSamples.get(focusedSampleId) : null;
